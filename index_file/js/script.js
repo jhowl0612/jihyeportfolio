88be3b5fbf12ac0ecdf6137ac983cfa49128e296
jihye0612 <sep0612@naver.com>
2019-06-04 21:46:23 +0900

    indexfile

---
+++ index_file/js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+	var ht=$(window).height();
+	$('#intro').height(ht);
+	
+	$(window).on('resize',function(){
+		var ht=$(window).height();
+		$('#intro').height(ht);
+	});
+	
+	
+	
+	
+	
+})
